877602331b44bed7c09476156b515c3129eeabfd
Cynthia Yeung <cyeung91@gmail.com>
2015-02-21 17:03:46 -0800

    Generated models and setup associations and validations

--- bloglab/app/models/author.rb
+++ bloglab/app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ActiveRecord::Base
-
+  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, length: {in: 4...20}
+  validates :age, numericality: true #checks if it's a number
+  has_many :posts, dependent: :destroy # Kill the author, burn the books
 end
--- bloglab/app/models/post.rb
+++ bloglab/app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-
+  validates :title, :body, :category, presence: true
+  validates :title, length: {minimum: 3}
+  belongs_to :author
 end
