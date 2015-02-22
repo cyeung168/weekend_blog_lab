72da290be640e02450310c4ef56789ca7e6ad624
Cynthia Yeung <cyeung91@gmail.com>
2015-02-21 17:05:26 -0800

    Refactored routes using nested resources

--- bloglab/config/routes.rb
+++ bloglab/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'authors#index'
+
+  resources :authors do #, except show // does all the restful routing for one resource
+    resources :posts, shallow: true
+  end
 
 end
